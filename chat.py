355e0a8b5192f7d6bde9988f9588c36e0185dd5a
freakie123 <ralphmensah123@gmail.com>
2019-07-31 20:19:02 +0000

    button added

--- chat.py
+++ chat.py
@@ -20,6 +20,10 @@ frame = tk.Frame(
 
 frame.pack(fill='both', expand='yes')
 
+
+recievelab = Label(master=frame, text="RECIEVED",fg ='white',bg='gray',bd='0')
+recievelab.pack(side="top")
+
 recieve = scrolltxt.ScrolledText(
     master=frame,
     wrap = tk.WORD,
@@ -34,10 +38,14 @@ recieve.pack(padx=10,pady=10,fill=tk.BOTH, expand=True)
 #score = Label(height=2, width=10, text="Recieved")
 #score.pack(side="top")
 
-send = Entry(master=frame,width=500,bg = 'black',fg ='white')
+send = Entry(master=frame,width=50,bg = 'black',fg ='white')
 sendlab = Label(master=frame, text="SEND",fg ='white',bg='gray',bd='0')
+butt=Button(master=frame,text='SEND',height=0)
+
 sendlab.pack(side="top")
-send.pack(padx=10,pady=10,side=TOP)
+butt.pack(side=RIGHT)
+send.pack(padx=10,pady=10,side=LEFT)
+
 
 #root.update()
 
